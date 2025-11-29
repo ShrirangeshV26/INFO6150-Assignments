b3a7594eff43fab8e93680603ae9d22ea0919387
ShrirangeshV26 <shrirangesh962@gmail.com>
2025-11-28 21:59:07 -0500

    Update JobListings.jsx

--- job-portal2/src/pages/JobListings.jsx
+++ job-portal2/src/pages/JobListings.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import { jobPosts } from "../data/jobPosts";
+import JobCard from "../components/JobCard";
 
 const JobListings = () => {
   return (
     <div style={{ padding: "20px" }}>
       <h1>Job Listings</h1>
-      <p>We will show job cards here using data and map().</p>
+      <p>Browse the latest job opportunities:</p>
+
+      {jobPosts.map((job) => (
+        <JobCard key={job.id} job={job} />
+      ))}
     </div>
   );
 };
