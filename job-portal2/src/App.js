9f8ea4f5955f6718dfecc8fa5e30f8133cae5929
ShrirangeshV26 <shrirangesh962@gmail.com>
2025-11-28 21:54:28 -0500

    Update App.js

--- job-portal2/src/App.js
+++ job-portal2/src/App.js
@@ -1,23 +1,12 @@
 import React from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
-function HomePage() {
-  return (
-    <div style={{ padding: "20px" }}>
-      <h1>React Job Portal</h1>
-      <p>Welcome to the Job Portal home page.</p>
-    </div>
-  );
-}
-
-function LoginPage() {
-  return (
-    <div style={{ padding: "20px" }}>
-      <h1>Login</h1>
-      <p>(We will connect this to your backend later.)</p>
-    </div>
-  );
-}
+import Home from "./pages/Home";
+import About from "./pages/About";
+import JobListings from "./pages/JobListings";
+import CompanyShowcase from "./pages/CompanyShowcase";
+import Contact from "./pages/Contact";
+import Login from "./pages/Login";
 
 function App() {
   return (
@@ -33,13 +22,28 @@ function App() {
         <Link to="/" style={{ marginRight: "15px" }}>
           Home
         </Link>
+        <Link to="/about" style={{ marginRight: "15px" }}>
+          About
+        </Link>
+        <Link to="/jobs" style={{ marginRight: "15px" }}>
+          Jobs
+        </Link>
+        <Link to="/companies" style={{ marginRight: "15px" }}>
+          Companies
+        </Link>
+        <Link to="/contact" style={{ marginRight: "15px" }}>
+          Contact
+        </Link>
         <Link to="/login">Login</Link>
       </nav>
 
-      {/* Routes */}
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/jobs" element={<JobListings />} />
+        <Route path="/companies" element={<CompanyShowcase />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
