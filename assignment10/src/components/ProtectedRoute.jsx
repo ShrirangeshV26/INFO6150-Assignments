fdd4c7324c5dc7b12c0aa23ba756dcea1c3102eb
ShrirangeshV26 <shrirangesh962@gmail.com>
2025-11-29 00:52:15 -0500

    Update ProtectedRoute.jsx

--- assignment10/src/components/ProtectedRoute.jsx
+++ assignment10/src/components/ProtectedRoute.jsx
@@ -1,11 +1,19 @@
-import React from "react";
+// src/components/ProtectedRoute.jsx
+import { useSelector } from "react-redux";
 import { Navigate } from "react-router-dom";
-import { isAuthenticated } from "../api/authService";
 
-const ProtectedRoute = ({ children }) => {
-  if (!isAuthenticated()) {
+const ProtectedRoute = ({ children, allowedTypes }) => {
+  const { user, token } = useSelector((state) => state.auth);
+
+  if (!token || !user) {
     return <Navigate to="/login" replace />;
   }
+
+  if (allowedTypes && !allowedTypes.includes(user.type)) {
+    // user is logged in but not allowed for this route
+    return <Navigate to="/" replace />;
+  }
+
   return children;
 };
 
