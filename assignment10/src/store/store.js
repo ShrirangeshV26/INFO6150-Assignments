ebb7662bf5ac5bb367d1f71c6fc75b7fbf6deaf3
ShrirangeshV26 <shrirangesh962@gmail.com>
2025-11-29 00:47:09 -0500

    Create store.js

---
+++ assignment10/src/store/store.js
@@ -0,0 +1,15 @@
+// src/store/store.js
+import { configureStore } from "@reduxjs/toolkit";
+import authReducer from "../slices/authSlice";
+import usersReducer from "../slices/usersSlice";
+import jobsReducer from "../slices/jobsSlice";
+
+const store = configureStore({
+  reducer: {
+    auth: authReducer,
+    users: usersReducer,
+    jobs: jobsReducer,
+  },
+});
+
+export default store;
